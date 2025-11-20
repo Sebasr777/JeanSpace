7cc1669dc17c734818bf9f8a27885e71a346421a
Sebasr777 <sebasrodriguez072002@gmail.com>
2025-11-19 22:51:14 -0500

    Omitir Commit

--- Proyecto de Herramientas/RutaProyectoHelper.cs
+++ Proyecto de Herramientas/RutaProyectoHelper.cs
@@ -15,24 +15,7 @@ namespace Proyecto_de_Herramientas
     {
         public static class RutaProyectoHelper
         {
-            public static string ObtenerRaizProyecto()
-            {
-                string carpetaActual = Application.StartupPath;
-
-                while (carpetaActual != null)
-                {
-                    string posibleRuta = Path.Combine(carpetaActual, "Images", "Usuarios");
-                    if (Directory.Exists(posibleRuta))
-                    {
-                        return carpetaActual;
-                    }
-
-                    carpetaActual = Directory.GetParent(carpetaActual)?.FullName;
-                }
-
-                // Si no se encuentra, usar la carpeta de inicio como fallback
-                return Application.StartupPath;
-            }
+           
         }
     }
 
